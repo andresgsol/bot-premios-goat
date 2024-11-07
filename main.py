e030907f346087cc52ff9d4336a74b722f8ed94e
Andres Garcia Solares <andy.garciasol@hotmail.com>
2024-11-07 10:12:22 +0100

    React to /goat messages

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import logging
 from telegram import Update
+from telegram.constants import ReactionEmoji
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
 logging.basicConfig(
@@ -8,13 +9,13 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+async def goat(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.set_message_reaction(chat_id=update.effective_chat.id, message_id=update.effective_message.id, reaction=ReactionEmoji.FIRE)
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(os.environ['TELEGRAM_APITOKEN']).build()
     
-    start_handler = CommandHandler('start', start)
+    start_handler = CommandHandler('goat', goat)
     application.add_handler(start_handler)
     
     application.run_polling()
